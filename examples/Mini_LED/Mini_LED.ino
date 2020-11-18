73193c585d137949d83e9935b8a47a5fb2021fb9
KKITC <frason@kkitc.net>
2020-11-18 17:25:47 +0800

    fix example: Mini_LED

--- examples/Mini_LED/Mini_LED.ino
+++ examples/Mini_LED/Mini_LED.ino
@@ -4,7 +4,7 @@
  *               We provide two controlling method, RGB and HSV mode.
  *
  * Author:      Frason Fan
- * modified 16 Dec 2019
+ * modified 17 Nov 2020
  *  
  * www.matrixrobotics.com
 */
@@ -14,36 +14,34 @@ float sat, val;
 
 void setup() {
   Mini.begin();
-  Serial.begin(9600);       
+  Serial.begin(115200);       
   Serial.println("\nMatrix MINI Test - RGB LED\n");
   Serial.println("Starting Up ... ");
   dir = 1;
   hue = 50;
   sat = 1.0;
   val = 1.0;
-  
   Mini.RGB1.setRGB(0, 0, 0);
   Mini.RGB2.setHSV(hue, sat, val);
-  
   randomSeed(analogRead(0));
   delay(2000);
 }
 
 void loop() {
   // Blink first RGB
-  if (millis()/600 % 2)
+  unsigned int i = millis();
+  Serial.print("Time: ");  Serial.println(i);
+  if (i / 600 % 2)
     Mini.RGB1.setRGB(255, 0, 0);
   else 
     Mini.RGB1.setRGB(0, 0, 255);
 
   // Color Hue for Second RGB
-  hue += dir * random(1, 5);
-  if( hue > 359 || hue < 0) dir = -dir;
-  hue = constrain(hue, 0, 359);
-  Mini.RGB2.setHSV(hue, sat, val);
-  
+  hue=millis() / 20 % 360;
+  Mini.RGB2.setHSV(hue,sat,val);
+
   Serial.print(" HUE: ");
   Serial.println(hue);
-
-  delay(30);
+  Serial.println("=====================================");
+  delay(10);
 }
