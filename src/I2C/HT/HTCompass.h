aab1e50d34494e5fe9efe4e5de282065484fcaf2
DCisHurt <roger33669@gmail.com>
2021-02-23 11:00:43 +0800

    fix compass

--- src/I2C/HT/HTCompass.h
+++ src/I2C/HT/HTCompass.h
@@ -3,22 +3,20 @@
 
 #include "../../utility/PCA954X.h"
 
-#define HT_COMPASS_ADDR 		0x01
+#define HT_COMPASS_ADDR 				0x01
 
-#define HT_COMPASS_REG_COMMAND	0x41
-#define HT_COMPASS_HEADING2		0x42
-#define HT_COMPASS_HEADING1		0x43
-#define HT_COMPASS_HEADING 		0x44
+#define HT_COMPASS_MODE					0x41
+#define HT_COMPASS_TWO_DEGREE_HEAD		0x42
+#define HT_COMPASS_ONE_DEGREE_ADDR		0x43
+#define HT_COMPASS_HEADING_L			0x44
 
 class HTCompass{
 private:
 	uint8_t _ch, _ver;
 public:
 	friend class MiniI2C;
-	void startCalibration();
-	bool finishCalibration();
-	uint8_t getHeading2();
-	uint16_t getHeading1();
+	void startCal();
+	bool finishCal();
 	uint16_t getHeading();
 };
 
