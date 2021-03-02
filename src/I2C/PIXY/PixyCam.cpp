aea4c42c2050a72bc2394835d6902a7ab8fa5399
RUI-LONG <m61216051116@gmail.com>
2021-03-02 16:45:38 +0800

    Added pixy block Usage

--- src/I2C/PIXY/PixyCam.cpp
+++ src/I2C/PIXY/PixyCam.cpp
@@ -1,49 +1,20 @@
 #include "PixyCam.h"
 
-uint8_t PIXYCam::getBlocks(bool wait=true, uint8_t sigmap=CCC_SIG_ALL, uint8_t maxBlocks=0xff){
+void PIXYCam::initCam(){
+  pixy.init();
+}
+
+
+Block PIXYCam::getBlocks(int n_th=0){
+  // grab blocks!
+  i2cMUXSelect(_ch, _ver);
+  pixy.ccc.getBlocks();
   
-  // blocks = NULL;
-  // numBlocks = 0;
-  // uint8_t* m_buf = (uint8_t *)malloc(CAM_BUFFERSIZE);
-
-  // TODO: 
-  // uint8_t m_buf[4];
-
-  // m_buf[0] = CCC_SIG_ALL;
-  // m_buf[1] = maxBlocks;
-
-  // int m_type = CCC_REQUEST_BLOCKS;
-  // int m_length = 2;
-  // m_buf[2] = m_type;
-  // m_buf[3] = m_length;
-
-
-  // // i2cWriteBuff(PIXY_I2C_DEFAULT_ADDR, m_buf, m_length+PIXY_SEND_HEADER_SIZE);
-
-  // m_buf[0] = PIXY_NO_CHECKSUM_SYNC&0xff;
-  // m_buf[1] = PIXY_NO_CHECKSUM_SYNC>>8;
-  // // int m_type = CCC_REQUEST_BLOCKS;
-  // // int m_length = 2;
-  // // m_buf[2] = m_type;
-  // // m_buf[3] = m_length;
-
-  // // // send whole thing -- header and data in one call
-  // // return m_link.send(m_buf, m_length+PIXY_SEND_HEADER_SIZE);
-  // int len = m_length+PIXY_SEND_HEADER_SIZE;
-
-  
-  // // i2cReadBuff(PIXY_I2C_DEFAULT_ADDR, m_buf, 4);
-
-  // m_type = m_buf[0];
-  // m_length = m_buf[1];
-  
-  // uint16_t csSerial = *(uint16_t *)&m_buf[2];
-  
-
-  // // i2cReadBuff(CAM_I2C_DEFAULT_ADDR, m_buf, m_length);
-
-  // dataBuf = i2cReadData(PIXY_I2C_DEFAULT_ADDR, 0x02, 4);
-  // Serial.println(dataBuf);
-
+  if (pixy.ccc.numBlocks){ 
+    return pixy.ccc.blocks[n_th];
+  }
+  else{
+    return _block;
+  }    
 }
 
--- src/I2C/PIXY/PixyCam.h
+++ src/I2C/PIXY/PixyCam.h
@@ -2,41 +2,18 @@
 #define _PIXYPIXY_H_
 
 #include "../../utility/PCA954X.h"
+#include "utility/Pixy2I2C.h"
 
-#define CCC_SIG1                     1 
-#define CCC_SIG2                     2
-#define CCC_SIG3                     4
-#define CCC_SIG4                     8
-#define CCC_SIG5                     16
-#define CCC_SIG6                     32
-#define CCC_SIG7                     64
-#define CCC_SIG_ALL                  0xff
-
-#define CCC_COLOR_CODES              128
-
-#define CCC_MAX_SIGNATURE                   7
-#define CCC_RESPONSE_BLOCKS                 0x21
-#define CCC_REQUEST_BLOCKS                  0x20
-
-
-#define PIXY_TYPE_RESPONSE_ERROR             0x03
-
-#define PIXY_RESULT_ERROR                    -1
-#define PIXY_RESULT_BUSY                     -2
-#define PIXY_RESULT_PROG_CHANGING            -6
-#define PIXY_BUFFERSIZE                      0x104
-#define PIXY_NO_CHECKSUM_SYNC                0xc1ae
-#define PIXY_I2C_DEFAULT_ADDR                0x54
-#define PIXY_I2C_MAX_SEND                    16 // don't send any more than 16 bytes at a time
-#define PIXY_SEND_HEADER_SIZE                4
 
 class PIXYCam{
-// private:
-    //TODO: Use _ch, _ver 
-    // uint8_t _ch, _ver;
+private:
+    Pixy2I2C pixy;
+	uint8_t _ch, _ver;
+    Block _block;
 public:
     friend class MiniI2C;
-    uint8_t getBlocks(bool wait=true, uint8_t sigmap=CCC_SIG_ALL, uint8_t maxBlocks=0xff);
+    void initCam();
+    Block getBlocks(int n_th);
 };
 
 #endif
