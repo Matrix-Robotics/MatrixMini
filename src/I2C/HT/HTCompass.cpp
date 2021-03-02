16f5da63a04497ae8308b74cad43d28800e08622
DCisHurt <roger33669@gmail.com>
2021-03-02 10:17:42 +0800

    fix compass

--- src/I2C/HT/HTCompass.cpp
+++ src/I2C/HT/HTCompass.cpp
@@ -1,22 +1,25 @@
 #include "HTCompass.h"
 
-void HTCompass::startCal(){
-	i2cWriteData(HT_COMPASS_ADDR, HT_COMPASS_MODE, 0x43);
-}
+// void HTCompass::startCal(){
+// 	i2cMUXSelect(_ch, _ver);
+// 	i2cWriteData(HT_COMPASS_ADDR, HT_COMPASS_MODE, 0x43);
+// }
 
-bool HTCompass::finishCal(){
-	i2cWriteData(HT_COMPASS_ADDR, HT_COMPASS_MODE, 0);
-	delay(5);
+// bool HTCompass::finishCal(){
+// 	i2cMUXSelect(_ch, _ver);
+// 	i2cWriteData(HT_COMPASS_ADDR, HT_COMPASS_MODE, 0);
+// 	delay(5);
 
-	if (i2cReadData(HT_COMPASS_ADDR, HT_COMPASS_MODE, 1) == 2){
-		return false;
-	}
-	else {
-		return true;
-	}
-}
+// 	if (i2cReadData(HT_COMPASS_ADDR, HT_COMPASS_MODE, 1) == 2){
+// 		return false;
+// 	}
+// 	else {
+// 		return true;
+// 	}
+// }
 
 uint16_t HTCompass::getHeading(){
+	i2cMUXSelect(_ch, _ver);
 	return (i2cReadData(HT_COMPASS_ADDR, HT_COMPASS_TWO_DEGREE_HEAD, 1)*2 +
 	 		i2cReadData(HT_COMPASS_ADDR, HT_COMPASS_ONE_DEGREE_ADDR, 1));
 }
