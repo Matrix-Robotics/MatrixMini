b210379ccf17a652afe52e167690ee2fd4793323
RUI-LONG <m61216051116@gmail.com>
2021-05-09 23:11:50 +0800

    Update example for PIXY driver

--- examples/I2C_Devices/Mini_PIXYcam/Mini_PIXYcam.ino
+++ examples/I2C_Devices/Mini_PIXYcam/Mini_PIXYcam.ino
@@ -14,54 +14,68 @@ void setup() {
   Mini.begin();
   Serial.println("\nMatrix Mini Test - PIXY2 Camera\n");
   Serial.println("PIXY2 Camera on I2C port 1 ");
+
+  // sigmap means allow signature range
+  // Ex: sigmap = 3, means allowed signatures are: 1, 2, 3.
+  Mini.I2C1.PIXYcam.pixyinit(3);
 }
+int i=0;
 
 void loop() {
+  // Get specific signature, with n_th largest block.
+  // EX: If you have signature 1 is blue, 2 is red. 
+  // Then the largest blue block's X is getblock(1, 1), largest red block's X is getblock(2, 1),
+  // The second largest blue block's X is getblock(1, 2), second largest red block's X is getblock(2, 2).
+  // Note: get block should be use before other functions.
+  // getblock(0, 1) means select the largest block with no specific signature.
+  bool isBlockValid = Mini.I2C1.PIXYcam.getblock(0, 1);
+  if (isBlockValid==true)
+  {
+    Serial.print("Num of Frames: ");
+    Serial.println(i);
+    i ++;
 
-  for (int i=0; i<2; i++){
-    //  Get n largest color block, captured by PIXY Cam.
-    //  0 is the largest, 1 is smaller then 0 color block, and so on.
-    Mini.I2C1.PIXYcam.getblock(i);
-    //  .print(): A member function that prints the detected object information to the serial port
-    Mini.I2C1.PIXYcam.block.print();
+    ////////////////////////////////
+    ////// PIXYCam Functions //////
+    ///////////////////////////////
 
-    Serial.print(i);
-    Serial.println(" largest color block");
+    // .getCountofBlock(): Count of received blocks, default max is 8 .
+    Serial.print("Count of blocks : ");
+    Serial.println(Mini.I2C1.PIXYcam.getCountofBlock());
 
-    //  .m_signature: The signature number of the detected object (1-7 for normal signatures)
-    Serial.print("Get block sig : ");
-    Serial.println(Mini.I2C1.PIXYcam.block.m_signature);
+    ///////////////////////////////////
+    ////// ColorBlock properties //////
+    ///////////////////////////////////
 
-    //  .m_x: The x location of the center of the detected object (0 to 316)
-    Serial.print("Get block x position: ");
-    Serial.println(Mini.I2C1.PIXYcam.block.m_x);
-    //  .m_y: The y location of the center of the detected object (0 to 208)
-    Serial.print("Get block y position: ");
-    Serial.println(Mini.I2C1.PIXYcam.block.m_y);
+    // .signature: The signature number of the detected object (1-7 for normal signatures)
+    Serial.print("block's signature : ");
+    Serial.println(Mini.I2C1.PIXYcam.block.signature);
 
-    //  .m_width: The width of the detected object (1 to 316)
-    Serial.print("Get block width: ");
-    Serial.println(Mini.I2C1.PIXYcam.block.m_width);
-    //  .m_height: The height of the detected object (1 to 208)
-    Serial.print("Get block height: ");
-    Serial.println(Mini.I2C1.PIXYcam.block.m_height);
+    // .x: The x location of the center of the detected object (0 to 316)
+    Serial.print("x position: ");
+    Serial.println(Mini.I2C1.PIXYcam.block.x);
+    // .y: The y location of the center of the detected object (0 to 208)
+    Serial.print("y position: ");
+    Serial.println(Mini.I2C1.PIXYcam.block.y);
 
-    //  .m_angle: The angle of the object detected object if the detected object is a color code (-180 to 180).
-    Serial.print("Get block angle: ");
-    Serial.println(Mini.I2C1.PIXYcam.block.m_angle);
+    // .width: The width of the detected object (1 to 316)
+    Serial.print("width: ");
+    Serial.println(Mini.I2C1.PIXYcam.block.width);
+    // .height: The height of the detected object (1 to 208)
+    Serial.print("height: ");
+    Serial.println(Mini.I2C1.PIXYcam.block.height);
 
-    //  .m_index :The tracking index of the block
-    Serial.print("Get block index: ");
-    Serial.println(Mini.I2C1.PIXYcam.block.m_index);
-    //  .m_age: The number of frames the block has been tracked.
+    // .angle: The angle of the object detected object if the detected object is a color code (-180 to 180).
+    Serial.print("angle: ");
+    Serial.println(Mini.I2C1.PIXYcam.block.angle);
 
-    Serial.print("Get block age: ");
-    Serial.println(Mini.I2C1.PIXYcam.block.m_age);
-
+    // .index :The tracking index of the block
+    Serial.print("index: ");
+    Serial.println(Mini.I2C1.PIXYcam.block.index);
+    // .age: The number of frames the block has been tracked.
+    Serial.print("age: ");
+    Serial.println(Mini.I2C1.PIXYcam.block.age);
     Serial.println("=================");
-    
     delay(100);
   }
-
-
 }
